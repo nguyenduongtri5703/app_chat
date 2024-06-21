452c26fdc161032a690449427dd78cb12954ef62
Duong Tri Nguyen <21130456@st.hcmuaf.edu.vn>
2024-06-21 19:19:46 +0700

    chat message design

--- src/component/chat/Converse.jsx
+++ src/component/chat/Converse.jsx
@@ -26,25 +26,65 @@ const Converse = () => {
             <img src="/info.png" alt=""/>
           </div>
         </div>
-        <div className="center"></div>
-        <div className="bottom">
-          <div className="icons">
-            <img src="/img.png" alt=""/>
-            <img src="/camera.png" alt=""/>
-            <img src="/mic.png" alt=""/>
+          <div className="center">
+              <div className="message">
+                  <img src="/avatar.png" alt=""/>
+                  <div className="texts">
+                      <p>Previously known as an outstanding Jinzhou Patroller,
+                          Yinlin is steady and reliable,
+                          yet harbors hidden depths of secrets.
+                      </p>
+                      <span>1 phút trước</span>
+                  </div>
+              </div>
+              <div className="message own">
+                  <div className="texts">
+                      <p>Previously known as an outstanding Jinzhou Patroller,
+                          Yinlin is steady and reliable,
+                          yet harbors hidden depths of secrets.
+                      </p>
+                      <span>1 phút trước</span>
+                  </div>
+              </div>
+              <div className="message">
+                  <img src="/avatar.png" alt=""/>
+                  <div className="texts">
+                      <p>Previously known as an outstanding Jinzhou Patroller,
+                          Yinlin is steady and reliable,
+                          yet harbors hidden depths of secrets.
+                      </p>
+                      <span>1 phút trước</span>
+                  </div>
+              </div>
+              <div className="message own">
+                  <div className="texts">
+                      <img src="https://scontent.fsgn4-1.fna.fbcdn.net/v/t39.30808-6/448839707_984915886354727_5128022529440101055_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGDKerHQJtjNom2Xp2NGrwn-Aw-xZTJspz4DD7FlMmynIZ_QeGu1m7OZ3PpJcdy3N0iPNLkFrQRJEnyYRzkN1HC&_nc_ohc=652BL-yu8eUQ7kNvgGqtneO&_nc_ht=scontent.fsgn4-1.fna&oh=00_AYCFhFxa2kS32uDZcpW9dkLZZuiAHCGODE7IWbRvyn22bQ&oe=667B32C8" alt=""/>
+                      <p>Previously known as an outstanding Jinzhou Patroller,
+                          Yinlin is steady and reliable,
+                          yet harbors hidden depths of secrets.
+                      </p>
+                      <span>1 phút trước</span>
+                  </div>
+              </div>
           </div>
-          <input type="text" placeholder="Nhập tin nhắn..."
-          value={text}
-          onChange={(e)=> setText(e.target.value)}
-          />
-          <div className="emoji">
-            <img src="/emoji.png" alt="" onClick={()=>setOpen(prev => !prev)}/>
-             <div className="picker">
-                 <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
-             </div>
+          <div className="bottom">
+              <div className="icons">
+                  <img src="/img.png" alt=""/>
+                  <img src="/camera.png" alt=""/>
+                  <img src="/mic.png" alt=""/>
+              </div>
+              <input type="text" placeholder="Nhập tin nhắn..."
+                     value={text}
+                     onChange={(e) => setText(e.target.value)}
+              />
+              <div className="emoji">
+                  <img src="/emoji.png" alt="" onClick={() => setOpen(prev => !prev)}/>
+                  <div className="picker">
+                      <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+                  </div>
+              </div>
+              <button className="sendButton">Gửi</button>
           </div>
-          <button className="sendButton">Gửi</button>
-        </div>
       </div>
   )
 }
