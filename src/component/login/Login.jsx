faaf26fddceb388c48f1cac3bc0c7535cccad8a2
Duong Tri Nguyen <21130456@st.hcmuaf.edu.vn>
2024-06-30 18:26:05 +0700

    Kết nối websocket, chức năng login

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -1,12 +1,45 @@
 import "./login.css"
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {toast} from "react-toastify";
+import WebSocketService from "../../WebSocketService";
 
-const Login = () => {
+const Login = ({ setUser }) => {
+
+    useEffect(() => {
+        WebSocketService.connect('ws://140.238.54.136:8080/chat/chat');
+
+        WebSocketService.registerCallback('LOGIN', (data) => {
+            console.log('Login response:', data);
+            if (data.RE_LOGIN_CODE) {
+                setUser(true);
+                toast.success("Đăng nhập thành công");
+            } else {
+                toast.error("Đăng nhập thất bại");
+            }
+        });
+
+        return () => {
+            WebSocketService.close();
+        };
+    }, []);
+
     const [avatar, setAvatar] = useState({
         file: null,
         url: ""
-    })
+    });
+
+    const [credentials, setCredentials] = useState({
+        user: "",
+        pass: ""
+    });
+
+    const handleInputChange = e => {
+        const {name, value} = e.target;
+        setCredentials({
+            ...credentials,
+            [name]: value
+        });
+    }
 
     const handleAvatar = e => {
         if (e.target.files[0]) {
@@ -18,17 +51,39 @@ const Login = () => {
     }
 
     const handleLogin = e => {
-        e.preventDefault()
-        toast.error("Xin chào")
+        e.preventDefault();
+        WebSocketService.sendMessage({
+            action: 'onchat',
+            data: {
+                event: 'LOGIN',
+                data: {
+                    user: credentials.user,
+                    pass: credentials.pass
+                }
+            }
+        });
     }
+
+    const handleReLogin = (user, code) => {
+        WebSocketService.sendMessage({
+            action: 'onchat',
+            data: {
+                event: 'RE_LOGIN',
+                data: {
+                    user: user,
+                    code: code
+                }
+            }
+        });
+    };
 
     return (
         <div className='login'>
             <div className="item">
                 <h2>Chào mừng trở lại,</h2>
                 <form onSubmit={handleLogin}>
-                    <input type="text" name="email" placeholder="Email"/>
-                    <input type="password" name="password" placeholder="Mật khẩu"/>
+                    <input type="text" name="user" placeholder="User" value={credentials.user} onChange={handleInputChange}/>
+                    <input type="password" name="pass" placeholder="Password" value={credentials.pass} onChange={handleInputChange}/>
                     <button>Đăng nhập</button>
                 </form>
             </div>
