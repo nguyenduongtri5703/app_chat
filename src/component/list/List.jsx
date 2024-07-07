6b1a423cc329568e2431b3dde88d6a19a8139c50
Lâm Hồng Phúc <21130477@st.hcmuaf.edu.vn>
2024-07-07 19:45:34 +0700

    Hiển thị tên của người dùng sau khi đăng nhập

--- src/component/list/List.jsx
+++ src/component/list/List.jsx
@@ -1,12 +1,14 @@
-import "./list.css"
+import "./list.css";
 import Userinfo from "./userInfo/Userinfo";
 import ChatList from "./chatList/ChatList";
-const List = () => {
+
+const List = ({ user }) => {
     return (
         <div className='list'>
-            <Userinfo/>
-            <ChatList/>
+            <Userinfo user={user} />
+            <ChatList user={user} />
         </div>
-    )
-}
+    );
+};
+
 export default List;
--- src/component/list/chatList/ChatList.jsx
+++ src/component/list/chatList/ChatList.jsx
@@ -1,51 +1,57 @@
-import "./chatList.css"
-import {useState} from "react";
+import "./chatList.css";
+import { useState, useEffect } from "react";
 import AddUser from "./addUser/addUser";
+import webSocketService from "../../../WebSocketService"; // Corrected path
 
-const ChatList = () => {
-    const [addMode, setAddMode] = useState(false)
+const ChatList = ({ user }) => {
+    const [addMode, setAddMode] = useState(false);
+    const [chatList, setChatList] = useState([]); // Initialize with an empty array
+
+    useEffect(() => {
+        const handleUserList = (data) => {
+            setChatList(data.userList || []); // Ensure it's an array
+        };
+
+        webSocketService.registerCallback('GET_USER_LIST', handleUserList);
+
+        webSocketService.sendMessage({
+            action: 'onchat',
+            data: {
+                event: 'GET_USER_LIST'
+            }
+        });
+
+        return () => {
+            webSocketService.close();
+        };
+    }, []);
+
     return (
         <div className='chatList'>
             <div className="search">
                 <div className="searchBar">
-                    <img src="/search.png" alt=""/>
-                    <input type="text" placeholder={"Tìm kiếm"}/>
+                    <img src="/search.png" alt="" />
+                    <input type="text" placeholder={"Tìm kiếm"} />
                 </div>
-                <img src={addMode ? "/minus.png" : "/plus.png"} alt="" className="add"
-                     onClick={() => setAddMode((prev) => !prev)}
+                <img
+                    src={addMode ? "/minus.png" : "/plus.png"}
+                    alt=""
+                    className="add"
+                    onClick={() => setAddMode((prev) => !prev)}
                 />
             </div>
-            <div className="item">
-                <img src="/avatar.png" alt=""/>
-                <div className="texts">
-                    <span>Dương Trí Nguyên</span>
-                    <p>Xin chào</p>
+            {chatList.map((chatUser, index) => (
+                <div key={index} className="item">
+                    <img src="/avatar.png" alt="" />
+                    <div className="texts">
+                        <span>{chatUser.username}</span>
+                        <p>{chatUser.message}</p>
+                    </div>
                 </div>
-            </div>
-            <div className="item">
-                <img src="/avatar.png" alt=""/>
-                <div className="texts">
-                    <span>Dương Trí Nguyên</span>
-                    <p>Xin chào</p>
-                </div>
-            </div>
-            <div className="item">
-                <img src="/avatar.png" alt=""/>
-                <div className="texts">
-                    <span>Dương Trí Nguyên</span>
-                    <p>Xin chào</p>
-                </div>
-            </div>
-            <div className="item">
-                <img src="/avatar.png" alt=""/>
-                <div className="texts">
-                    <span>Dương Trí Nguyên</span>
-                    <p>Xin chào</p>
-                </div>
-            </div>
-            {addMode && <AddUser/>}
+            ))}
+            {addMode && <AddUser />}
         </div>
-    )
-}
+    );
+};
 
 export default ChatList;
